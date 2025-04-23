62a8e7163a0a5f546ebd6ca533003fe6fc44e7ca
Aimane yousr <aimaneyousr@gmail.com>
2025-04-23 15:28:00 +0100

    Implement -nnnnn flag support in built-in echo command

--- builtins/echo.c
+++ builtins/echo.c
@@ -1,6 +1,18 @@
 #include "builtins.h"
 #include "../parssing/minishell.h"
 
+static int is_n_flag(const char *flag)
+{
+	int i;
+
+	if (!flag || flag[0] != '-' || flag[1] != 'n')
+		return (0);
+	i = 1;
+	while (flag[i] == 'n')
+		i++;
+	return (flag[i] == '\0');
+	
+}
 int	bi_echo(t_cmd *c, s_env **env)
 {
 	int		i;
@@ -9,11 +21,10 @@ int	bi_echo(t_cmd *c, s_env **env)
 	(void)env;
 	i = 1;
 	nl = 1;
-	if (c->argv[1] && !ft_strcmp(c->argv[1], "-n"))
+	while (c->argv[i] && is_n_flag(c->argv[i]))
 	{
 		nl = 0;
-		while (c->argv[i] && !ft_strcmp(c->argv[i], "-n"))
-			i++;
+		i++;
 	}
 	// Iterate through arguments and append a space between them for proper formatting
 	while (c->argv[i])
