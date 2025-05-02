20e96f5db97ee8c50ac7d94528740af94f700783
Aimane yousr <aimaneyousr@gmail.com>
2025-05-02 17:59:45 +0100

    Refactor builtin dispatch mechanism and update function pointer typedefs

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -3,7 +3,21 @@
 # include "../exec/exec.h"
 # include "../parssing/minishell.h"
 
-typedef int (*t_bifn)(t_cmd*, s_env**);
+extern s_env    *g_env_list;
+extern s_tokens *g_tokens;
+extern t_cmd    *g_cmds;
+
+/* Builtin function pointer */
+typedef int (*t_builtin_fn)(t_cmd *cmd, s_env **env);
+
+/* Dispatch table for builtins */
+typedef struct s_bdispatch {
+    char          *name;
+    t_builtin_fn   fn;
+}   t_bdispatch;
+
+/* External declaration of the builtin dispatch table */
+extern const t_bdispatch  g_builtins[];
 
 int   bi_echo(t_cmd *c, s_env **env);
 int   bi_cd(t_cmd *c, s_env **env);
@@ -17,7 +31,6 @@ void free_env_list(s_env *env);
 void free_tokens(s_tokens *tok);
 void free_cmd(t_cmd *cmd);
 void cleanup_and_exit(int code);
-
 s_env *find_env_node(s_env *env, const char *key);
 void set_env_var(s_env **env, const char *k, const char *v);
 void unset_env_var(s_env **env, const char *key);
