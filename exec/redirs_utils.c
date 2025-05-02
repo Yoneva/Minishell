591013c8dcde5d1ea73cbd9ef6aa34b3038666e6
Aimane yousr <aimaneyousr@gmail.com>
2025-05-02 18:00:56 +0100

    Add redirection utility function to handle command input/output redirections

---
+++ exec/redirs_utils.c
@@ -0,0 +1,38 @@
+#include "exec.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int	apply_redirs(t_cmd *c)
+{
+    int fd;
+    int target_fd;
+    int i;
+
+    i = 0;
+    while (i < c->n_redir)
+    {
+        if (c->redir[i].type == N_OUTPUT_SIGN)
+            fd = open(c->redir[i].filename, O_CREAT | O_TRUNC | O_WRONLY | 0644);
+        else if (c->redir[i].type == N_APPEND_SIGN)
+            fd = open(c->redir[i].filename, O_CREAT | O_APPEND | O_WRONLY, 0644);
+        else if (c->redir[i].type == N_INPUT_SIGN)
+            fd = open(c->redir[i].filename, O_RDONLY);
+        else
+            fd = -1;
+        if (c->redir[i].type == N_INPUT_SIGN)
+            target_fd = STDIN_FILENO;
+        else
+            target_fd = STDOUT_FILENO;
+        if (fd < 0 || dup2(fd, target_fd) < 0)
+        {
+            perror(c->redir[i].filename);
+            if (fd >= 0)
+                close(fd);
+            return (1);
+        }
+        close(fd);
+        i++;
+    }
+	return (0);
+}
