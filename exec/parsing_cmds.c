e36c855177ab1489aaf07abd60a93eb89796b372
Aimane yousr <aimaneyousr@gmail.com>
2025-05-05 13:52:59 +0100

    Remove unused ft_strcmp function from parsing_cmds.c to avoid redundency

--- exec/parsing_cmds.c
+++ exec/parsing_cmds.c
@@ -2,21 +2,6 @@
 #include "../builtins/builtins.h"
 #include "exec.h"
 
-
-int ft_strcmp(const char *s1, const char *s2)
-{
-	while (*s1 && *s2)
-    {
-		if ((unsigned char)*s1 != (unsigned char)*s2)
-        {
-			return ((unsigned char)*s1 - (unsigned char)*s2);
-        }
-        s1++;
-        s2++;
-    }
-	return ((unsigned char)*s1 - (unsigned char)*s2);
-}
-
 static int  is_redir(int sign)
 {
     return (sign == N_INPUT_SIGN || sign == N_OUTPUT_SIGN
