9400403ec9fcc89d499ab50ddd2292f955799803
Aimane yousr <aimaneyousr@gmail.com>
2025-05-20 11:12:03 +0100

    Implement sorting of environment variables in bi_export function

--- builtins/export.c
+++ builtins/export.c
@@ -5,18 +5,43 @@ int   bi_export(t_cmd *c, s_env **env)
 {
     int     i;
     char    *eq;
+    int     count;
+    s_env   **env_array = NULL;
+    s_env   *node;
 
     i = 1;
     if (!c->argv[i])
     {
-        s_env *node = *env;
+        count = count_env_vars(*env);
+        env_array = malloc(sizeof(s_env*) * count);
+        if (!env_array)
+        {
+            perror("export: malloc");
+            return 1;
+        }
+        // fill the array
+        node = *env;
+        i = 0;
         while (node)
         {
-            printf("%s=%s\n", node->data, node->value);
+            env_array[i++] = node;
             node = node->next;
         }
+        sort_env_vars(env_array, count);
+        i = 0;
+        while (i < count)
+        {
+            ft_putstr_fd("declare -x ", STDOUT_FILENO);
+            ft_putstr_fd(env_array[i]->data, STDOUT_FILENO);
+            ft_putstr_fd("=\"", STDOUT_FILENO);
+            ft_putstr_fd(env_array[i]->value, STDOUT_FILENO);
+            ft_putstr_fd("\"\n", STDOUT_FILENO);
+            i++;
+        }
+        free(env_array);
         return (0);
     }
+    i = 1;
     while (c->argv[i])
     {
         eq = ft_strchr(c->argv[i], '=');
