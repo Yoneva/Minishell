3769466cd785c4767ab40768de92817436d581de
Aimane yousr <aimaneyousr@gmail.com>
2025-05-20 11:11:28 +0100

    Add functions to sort and count environment variables

--- builtins/env_helpers.c
+++ builtins/env_helpers.c
@@ -1,6 +1,45 @@
 #include "builtins.h"
 #include "../parssing/minishell.h"
 
+void sort_env_vars(s_env **env_array, int count)
+{
+    int     i;
+    int     j;
+    s_env   *temp;
+
+    i = 0;
+    while (i < count - 1)
+    {
+        j = 0;
+        while (j < count - i - 1)
+        {
+            if (ft_strcmp(env_array[j]->data, env_array[j + 1]->data) > 0)
+            {
+                temp = env_array[j];
+                env_array[j] = env_array[j + 1];
+                env_array[j + 1] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+int count_env_vars(s_env *env)
+{
+    int count;
+
+    count = 0;
+    s_env *node = env;
+    while (node)
+    {
+        count++;
+        node = node->next;
+    }
+    return count;
+    
+}
+
 s_env *find_env_node(s_env *env, const char *key)
 {
     while (env)
